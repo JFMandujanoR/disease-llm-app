6c6e85a4d2d2bd0f4415e838de34ac73e1844a68
Juan Francisco Mandujano R <juanipnc6@gmail.com>
2025-09-24 20:11:25 -0700

    test

--- backend/ingest.py
+++ backend/ingest.py
@@ -6,7 +6,7 @@ DATA_DIR.mkdir(exist_ok=True)
 
 URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/" \
       "csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-
+ 
 def ingest():
     df = pd.read_csv(URL)
     df_long = df.melt(
